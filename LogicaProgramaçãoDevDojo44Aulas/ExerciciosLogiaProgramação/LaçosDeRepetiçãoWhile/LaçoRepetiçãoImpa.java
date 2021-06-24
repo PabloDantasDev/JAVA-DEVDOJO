ef186f8e4cb43cbaaca0e3ab52c4299b47673f96
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 12:12:17 -0300

    Laço Repetição Impa Com While

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoWhile/LaçoRepetiçãoImpa.java
@@ -0,0 +1,18 @@
+package LaçosDeRepetiçãoWhile;
+
+class LaçoRepetiçãoImpa {
+
+	public static void main(String[] args) {
+int numeroImpa=1;
+		
+		while(numeroImpa<=50) {
+			
+			System.out.println(numeroImpa);
+			numeroImpa = numeroImpa +2;
+			
+			
+		}
+
+	}
+
+}
