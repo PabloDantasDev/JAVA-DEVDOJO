69b3ec99d6863db3249a1aeb6e04c7e849fe8a4a
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 19:24:13 -0300

    Aula 01 Introdução a Laço de Repetição FOR.

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoFors/Aula01IntroduçãoFor.java
@@ -0,0 +1,12 @@
+package LaçosDeRepetiçãoFors;
+
+class Aula01IntroduçãoFor {
+
+	public static void main(String[] args) {
+		for(int contador=1;contador<=10;contador=contador+2) {
+			System.out.println(contador);
+		}
+		
+	}
+
+}
