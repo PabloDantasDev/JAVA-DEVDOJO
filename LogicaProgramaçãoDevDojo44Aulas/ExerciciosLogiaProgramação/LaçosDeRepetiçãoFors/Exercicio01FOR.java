260adf0cd03ba23b7e5ade8db6c76fe1772b15d3
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 20:48:56 -0300

    Exercicio 01 Laço de Repetição FOR - Fazer um Laço onde comece em 15 e seja <= 200 e sem numero inteiro e um quadrado perfeito.

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoFors/Exercicio01FOR.java
@@ -0,0 +1,25 @@
+/*Faça um laço onde comece a contar do 15 ao 200 e seja um quadrado perfeito
+ 
+  */
+ 
+
+package LaçosDeRepetiçãoFors;
+
+class Exercicio01FOR {
+
+	public static void main(String[] args) {
+		
+		for(int numeroI =15; numeroI<=200;numeroI++)  {
+			
+			System.out.println(numeroI*numeroI);
+			
+			
+			
+		}
+		
+		
+		
+
+	}
+
+}
