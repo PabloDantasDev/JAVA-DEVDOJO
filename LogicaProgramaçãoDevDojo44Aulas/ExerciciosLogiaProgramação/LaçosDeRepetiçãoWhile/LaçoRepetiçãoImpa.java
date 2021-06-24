a71b139694f74b09ce61311dc588b1ef7d2e332d
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 13:09:29 -0300

    att

--- LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoWhile/LaçoRepetiçãoImpa.java
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoWhile/LaçoRepetiçãoImpa.java
@@ -5,7 +5,7 @@ class LaçoRepetiçãoImpa {
 	public static void main(String[] args) {
 int numeroImpa=1;
 		
-		while(numeroImpa<=50) {
+		while(numeroImpa<=100) {
 			
 			System.out.println(numeroImpa);
 			numeroImpa = numeroImpa +2;
