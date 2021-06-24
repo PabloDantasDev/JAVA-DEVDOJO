fe49ae9a76a196027f3da193d7da23222d169e21
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 13:38:27 -0300

    Laço de Repetição de 0 atá numero digitado

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoWhile/LaçoRepetiçãoDe0AtéValorDigitado.java
@@ -0,0 +1,30 @@
+package LaçosDeRepetiçãoWhile;
+import java.util.Scanner;
+class LaçoRepetiçãoDe0AtéValorDigitado {
+
+	public static void main(String[] args) {
+		Scanner ler =new Scanner(System.in);
+		
+		int valorDigitado;
+		int i =0;
+		
+		
+		
+		
+		System.out.println("Me Informe um Valor?");
+		valorDigitado = ler.nextInt();
+		
+		while(i<=valorDigitado) {
+			if(i % 2 != 0) {
+			System.out.println("i = " + i);
+			}
+			
+				i = i + 1;
+		}
+		
+			
+				
+			}
+}
+
+
