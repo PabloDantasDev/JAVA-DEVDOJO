1f4918a46e6ff1c030b7bbb3f159c1dcd71ea9a7
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 10:57:22 -0300

    Aula 1 Introdução a Laço de Repetição While

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoWhile/Aula01IntroduçãoWhile.java
@@ -0,0 +1,26 @@
+package LaçosDeRepetiçãoWhile;
+
+class Aula01IntroduçãoWhile {
+
+	public static void main(String[] args) {
+	
+		
+		int contador=25;
+		
+		while(contador<=50) {
+			
+			System.out.println(contador);
+			contador = contador +1;
+			
+			
+		}
+			
+
+		
+		
+		
+		
+	
+	}
+
+}
