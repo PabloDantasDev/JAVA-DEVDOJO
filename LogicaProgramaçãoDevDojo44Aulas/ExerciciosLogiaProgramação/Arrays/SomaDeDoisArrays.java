e2e774ac6b44134160acf2b014d9bc7d4372a566
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-25 13:51:07 -0300

    AULA 02 SOMA DE DOIS ARRAYS

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/Arrays/SomaDeDoisArrays.java
@@ -0,0 +1,53 @@
+//SOMA DE 2 arrays 
+
+
+package Arrays;
+import java.util.Scanner;
+class SomaDeDoisArrays {
+
+	public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
+		
+	
+		
+		int [] v1 = new int [9];
+		int [] v2 = new int [9];
+		int [] somaV1eV2 = new int[9];
+		
+		
+		
+		for (int i = 0; i < v1.length; i++) {
+
+			System.out.println("Informe um Numero V1");
+			 v1[i]=ler.nextInt();
+
+			 System.out.println("Informe Outro Numero V2");
+			 v2[i]=ler.nextInt();
+			 
+			 somaV1eV2[i]=v1[i]*v2[i];
+		
+			 
+		}
+			
+		
+			for (int i = 0; i < v1.length; i++) {
+			
+			for(int contador = 0; i < v1.length; i++){
+				System.out.print(v1[i] + "|\n");
+				System.out.print(v2[i] + "|");
+				System.out.println("VR " + (v1[i]*v2[i]));}
+
+			
+				System.out.println("FIM DA OPERAÇÃO");
+					 {{{{{{
+					
+					}}}}}}}}}
+			
+		
+		
+			
+
+			  
+				  			 
+		 
+
