06fd489158dead2c6647d2145f1959388c955c72
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 21:42:37 -0300

    Exercicio 02 Tabuada 1 a 10.

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/LaçosDeRepetiçãoFors/Exercicio02Tabuada.java
@@ -0,0 +1,20 @@
+package LaçosDeRepetiçãoFors;
+
+class Exercicio02Tabuada {
+
+public static void main(String[] args) {
+
+for(int i = 1; i<=10;i++) {
+	System.out.println("-------------------");
+	System.out.println("Tabuada de "+ i);
+	System.out.println("-------------------");
+for( int j=1; j <=10; j++) {
+	
+System.out.println(j + "x" + i + "=" + j*i );
+			 
+   }
+System.out.println("Fim da TABUADA");  
+}
+ }
+}
+
