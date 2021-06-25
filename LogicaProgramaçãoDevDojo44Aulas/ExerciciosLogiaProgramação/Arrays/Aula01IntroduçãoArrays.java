85d1dd908bdbd25939de0838ff16984cfcc3fab2
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-24 22:45:28 -0300

    Aula 01 Introdução a Arrays

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/Arrays/Aula01IntroduçãoArrays.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+class Aula01IntroduçãoArrays {
+
+	public static void main(String[] args) {
+	double [] notas = new double[4];
+	notas[0]=10;
+	notas[1]=15;
+	notas[2]=17;
+	notas[3]=15;
+	for (int i = 0; i <4; i++) {
+		
+		System.out.println(notas[i]);
+	}
+	
+	
+	}
+ 
+}
