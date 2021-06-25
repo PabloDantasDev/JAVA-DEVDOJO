0807191b4f936e00772098f7629d0b42add9d2b2
lRickDantas <lRickDantas@LAPTOP-RLPG4VG8>
2021-06-25 15:16:37 -0300

    AULA 1 INTRODUÇÃO A ARRAYS MULTIDIMENSIONAIS

---
+++ LogicaProgramaçãoDevDojo44Aulas/ExerciciosLogiaProgramação/ArraysMULTIDIMENSIONAIS/Aula01IntroduçãoArraysMultidimensionais.java
@@ -0,0 +1,15 @@
+package ArraysMULTIDIMENSIONAIS;
+
+class Aula01IntroduçãoArraysMultidimensionais {
+
+	public static void main(String[] args) {
+		int [][] ArrayCom2DimiDimensao = new int [2][2];
+		ArrayCom2DimiDimensao[0][0]=25;
+		ArrayCom2DimiDimensao[0][1]=20;
+		ArrayCom2DimiDimensao[1][0]=35;
+		ArrayCom2DimiDimensao[1][1]=80;
+		
+		System.out.println(ArrayCom2DimiDimensao[0][1]+ArrayCom2DimiDimensao[1][1]);
+	}
+
+}
